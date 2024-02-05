11dff9895bea980fafc2cdb523a3a6834c92e58d
toomach <yaroshpavlo@gmail.com>
2024-02-05 17:58:39 +0200

    1

--- js/task-1.js
+++ js/task-1.js
@@ -120,16 +120,30 @@
 // return - повертає значення функції
 
 
-function makeTransaction(quantity, pricePerDroid) {
-    const totalprice = quantity * pricePerDroid;
-    const message = (`You ordered ${quantity} droids worth ${totalprice} credits!`);
-    return message;
-}
+// function makeTransaction(quantity, pricePerDroid) {
+//     const totalprice = quantity * pricePerDroid;
+//     const message = (`You ordered ${quantity} droids worth ${totalprice} credits!`);
+//     return message;
+// }
 
-makeTransaction(5, 3000);
-makeTransaction(3, 1000);
-makeTransaction(10, 500);
+// makeTransaction(5, 3000);
+// makeTransaction(3, 1000);
+// makeTransaction(10, 500);
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+// =========================практика=============================
+
+// Якщо змінна a дорівнює 10, виведіть 'Вірно', інакше виведіть 'Невірно'.
+
+// let a = 10;
+// if (a === 10) {
+//     console.log("вірно");
+// } else {
+//     console.log("невіро");
+// }
+
+const a === 10 ? "вірно" : "невірно";
+console.log(a);
